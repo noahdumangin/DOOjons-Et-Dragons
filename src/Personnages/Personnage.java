9f8f1793911172e0875b971f4d56e9435f813282
Nolan Denis <nolandenis971@gmail.com>
2025-05-06 09:45:17 +0200

    test commit

---
+++ src/Personnages/Personnage.java
@@ -0,0 +1,14 @@
+package Personnages;
+
+import java.util.ArrayList;
+
+public class Personnage
+{
+    private String m_nom;
+    private Race m_race;
+    private Job m_class;
+    private Stat m_stat;
+    private ArrayList<Item> m_inventory;
+    private Armor m_armor;
+    private Weapon m_weapon
+}
