2337183ed79f963b05d068cebffbe39b2104f290
Noah Dumangin <noah.dumangin@unistra.fr>
2025-05-21 17:19:34 +0200

    sort Arme magique

--- rendu4/src/sorts/Arme_magique.java
+++ rendu4/src/sorts/Arme_magique.java
@@ -1,9 +1,16 @@
 package sorts;
 
 import personnages.Personnage;
+import items.Weapon;
 
 public class Arme_magique extends Sort{
-    public void lancer(Personnage Pchoisi){
-        
+    public void lancer(Weapon Armechoisi){
+
+        int damage = Armechoisi.add_dmg(Armechoisi);
+        int portee= Armechoisi.add_atk_reach(Armechoisi);
+
+
+        System.out.println("L'arme " + Armechoisi.getNom() + " a été améliorée, " + "\n"+
+                "elle fait "+ damage +" dégâts et a une portée de "+portee+"!"+"\n");
     }
 }
