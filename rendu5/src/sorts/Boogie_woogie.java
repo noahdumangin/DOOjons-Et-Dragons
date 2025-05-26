94048834502030a2d937f2eb86d0b96095637bb1
dumangin <noah.dumangin@etu.unistra.fr>
2025-05-26 17:17:43 +0200

    boogie woogie

--- rendu5/src/sorts/Boogie_woogie.java
+++ rendu5/src/sorts/Boogie_woogie.java
@@ -1,5 +1,10 @@
 package sorts;
 
+import personnages.Personnage;
+
 public class Boogie_woogie extends Sort{
 
+    public void Lancer(Personnage PersoChoisi1, Personnage PersoChoisi2){
+
+    }
 }
