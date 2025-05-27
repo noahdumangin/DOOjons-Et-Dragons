9479d3c73533595463fe15acd48713e44319ef66
dumangin <noah.dumangin@etu.unistra.fr>
2025-05-27 11:41:12 +0200

    sort boogie woogie

--- rendu5/src/sorts/Arme_magique.java
+++ rendu5/src/sorts/Arme_magique.java
@@ -6,7 +6,7 @@ public class Arme_magique extends Sort{
     public void lancer(Weapon Armechoisi){
 
         int damage = Armechoisi.add_dmg(Armechoisi);
-        int portee= Armechoisi.add_atk_reach(Armechoisi);
+        int portee = Armechoisi.add_atk_reach(Armechoisi);
 
 
         System.out.println("L'arme " + Armechoisi.getNom() + " a été améliorée, " + "\n"+
