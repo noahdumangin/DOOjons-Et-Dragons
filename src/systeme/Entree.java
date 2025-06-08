e73543890a79eb098ef96b5130449cbcd0d55cce
qmey <qmey@iin-prt-etu90>
2025-06-08 15:33:58 +0200

    ajout de lireInt

--- src/systeme/Entree.java
+++ src/systeme/Entree.java
@@ -5,6 +5,23 @@ import java.util.Scanner;
 public class Entree {
 
     public Scanner scanner = new Scanner(System.in);
+    Affichage affichage = new Affichage();
+    public int lireInt(String message) {
+        int valeur;
+        while (true) {
+            affichage.afficher(message);
+            try {
+                valeur = Integer.parseInt(scanner.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Erreur : Veuillez entrer un entier valide.");
+            }
+        }
+        return valeur;
+    }
 
+    public void fermer() {
+        scanner.close();
+    }
 }
 
